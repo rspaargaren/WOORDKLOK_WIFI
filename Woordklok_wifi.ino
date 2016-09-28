5900daf294a8e5be393e6344b74989ba7667a4bb
rspaargaren <github@speijkers.demon.nl>
2016-09-28 22:54:23 +0200

    Update Woordklok_wifi.ino
    Nieuwe update, 
    Aantal settings worden in EEPROM weggeschreven en ingelezen bij opstarten
    Tijd wordt bij setting auto update ON automatische ingesteld (Incl. zomer / winter tijd compensatie)
    Tijd wordt automatisch geupdate op de gekozen tijd

--- Woordklok_wifi.ino
+++ Woordklok_wifi.ino
@@ -1,5 +1,5 @@
 /*********
-ESP WOORDKLOK 
+ESP WOORDKLOK V0.1.0
 *********/
 
 #include <ESP8266WiFi.h>
@@ -11,14 +11,17 @@ ESP WOORDKLOK
 #include <DNSServer.h>
 #include <EEPROM.h>
 #include <WiFiUdp.h>
+//#include <NtpClientLib.h>
+#include <TimeLib.h>
 //#include <ESP8266HTTPClient.h>
 //#include <TimeLib.h>
+#include<TimeAlarms.h>
 
 MDNSResponder mdns;
 
 // Replace with your network credentials
 const char* host = "esp8266-webupdate";
-const char* ssid = "XXXXXXXX";
+const char* ssid = "XXXXXXX";
 const char* password = "XXXXXXXX";
 
 // TIME SERVER PARAMETERS
@@ -37,10 +40,15 @@ String Clock_Light_abs;
 String Clock_Mode = "110";
 String Clock_TComp = "-10";
 String Clock_Sound = "0";
+String Clock_Notat = "0";
+int Clock_Lmax = 100;
+int Clock_Lmin = 10;
 String Debug1;
 String Debug2;
 String Debug3;
-Boolean Wintertijd = "false";
+String Debug4;
+String Debug5;
+int Daylightsaving = 3600;
 
 ESP8266WebServer server(80);
 ESP8266HTTPUpdateServer httpUpdater;
@@ -50,10 +58,16 @@ String webPage = "";
 void handle_sound() {
   // get the value of request argument "state" and convert it to an int
   int state = server.arg("state").toInt();
-  if (state == 1) 
+  if (state == 1) {
   Serial.println("SET SOUND 1");
+  EEPROM.write(310,1);
+  }
   else
+  {
   Serial.println("SET SOUND 0");
+  EEPROM.write(310,0);
+  }
+  EEPROM.commit();
   server.send(200, "text/html", webPage);
   delay(1000);
 }
@@ -61,10 +75,16 @@ void handle_sound() {
 void handle_notat() {
   // get the value of request argument "state" and convert it to an int
   int state = server.arg("state").toInt();
-  if (state == 1) 
+  if (state == 1) {
   Serial.println("SET NOTAT 1");
+  EEPROM.write(320,1);
+  }
   else
+  {
   Serial.println("SET NOTAT 5");
+  EEPROM.write(320,5);
+  }
+  EEPROM.commit();
   server.send(200, "text/html", webPage);
   delay(1000);
 }
@@ -80,6 +100,8 @@ void handle_lmax() {
   // get the value of request argument "state" and convert it to an int
   String lmax = server.arg("LMAX");
   Serial.println("SET LMAX " + lmax);
+  EEPROM.write(330,lmax.toInt());
+  EEPROM.commit();
   server.send(200, "text/html", webPage);
   delay(1000);
 }
@@ -87,6 +109,8 @@ void handle_lmin() {
   // get the value of request argument "state" and convert it to an int
   String lmin = server.arg("LMIN");
   Serial.println("SET LMIN " + lmin);
+  EEPROM.write(340,lmin.toInt());
+  EEPROM.commit();
   server.send(200, "text/html", webPage);
   delay(1000);
 }
@@ -101,6 +125,9 @@ void handle_modes() {
   // get the value of request argument "state" and convert it to an int
   String state = server.arg("mode");
   Serial.println("SET MODE " + state);
+  EEPROM.write(350,state.toInt());
+  EEPROM.commit();
+  Debug5 = String(EEPROM.read(350));
   server.send(200, "text/html", webPage);
   delay(1000);
 }
@@ -116,6 +143,7 @@ void handle_maninp() {
 void handle_autotime() {
   // get the value of request argument "state" and convert it to an int
   int state = server.arg("mode").toInt();
+  String tijd = server.arg("tijd");
   //Serial.println(ManInput);
   //Debug3 = state;
   if (state == 1) {
@@ -128,9 +156,21 @@ void handle_autotime() {
   }
   EEPROM.commit();
   server.send(200, "text/html", webPage);
-  delay(1000);
-  
+  //delay(1000);
+  String tijd_uur = getValue(tijd, ':', 0);
+  String tijd_min = getValue(tijd, ':', 1);
+  Alarm.alarmRepeat(tijd_uur.toInt(),tijd_min.toInt(),1, UpdateClockTime);
+  EEPROM.write(400,tijd_uur.toInt());
+  EEPROM.write(410,tijd_min.toInt());
+  EEPROM.commit();
 }
+
+void handle_test(){
+      SetClockTime();
+      FormatTime();
+      server.send(200, "text/html", webPage);
+}
+
 
 void Read_Serial(){
  //   Serial.println("Ready");
@@ -144,8 +184,8 @@ void Read_Serial(){
 }
 
  void getclocksettings() {
-  // Get the settings of the clock by sending out some serial commands and waiting for the response
- //Ser_Input = Serial.readString();// read the incoming data as string and do nothing to clear the buffer!
+ // Get the settings of the clock by sending out some serial commands and waiting for the response
+ Ser_Input = Serial.readString();// read the incoming data as string and do nothing to clear the buffer!
   
  Serial.println("GET TIME");
  Read_Serial();
@@ -157,9 +197,9 @@ void Read_Serial(){
  Clock_Light_abs = Ser_Input.substring(35,38);
  //Serial.println(Clock_Light_rel);
  //Serial.println(Clock_Light_abs);
- Serial.println("GET MODE");
- Read_Serial();
- Clock_Mode = Ser_Input.substring(14,17);
+ //Serial.println("GET MODE");
+ //Read_Serial();
+ //Clock_Mode = Ser_Input.substring(14,17);
  //Serial.println(Clock_Mode);
  Serial.println("GET TCOMP");
  Read_Serial();
@@ -177,14 +217,17 @@ void Read_Serial(){
   
 void Update_web(){
   webPage = "";
-  webPage += "<h1>ESP8266 WOORDKLOK VERSIE OTA</h1>";
+  webPage += "<h1>ESP8266 WOORDKLOK VERSIE AUTO SETTINGS</h1>";
   if (Clock_Sound =="0")
   webPage += "<form action='Sound'>SOUND: <input type='radio' name='state' value='1'>On<input type='radio' name='state' value='0' checked>Off <input type='submit' value='Submit'></form>";
   else
   webPage += "<form action='Sound'>SOUND: <input type='radio' name='state' value='1' checked>On<input type='radio' name='state' value='0'>Off <input type='submit' value='Submit'></form>";
   // Geen uitlees mogelijkheden voor de notatie...
+  if (Clock_Notat =="1")
   webPage += "<form action='Notat'>NOTATIE: <input type='radio' name='state' value='1' checked>1 min<input type='radio' name='state' value='0'>5 min<input type='submit' value='Submit'></form>";
-  webPage += "<form action='Time'>TIME: <input type='text' name='tijd' value='" + Clock_Time + "'><input type='submit' value='Submit'></form>";
+  else
+  webPage += "<form action='Notat'>NOTATIE: <input type='radio' name='state' value='1' >1 min<input type='radio' name='state' value='0' checked>5 min<input type='submit' value='Submit'></form>";
+  webPage += "<form action='Time'>CURRENT CLOCK TIME: <input type='text' name='tijd' value='" + Clock_Time + "'><input type='submit' value='Submit'></form>";
   webPage += "<form action='Time'>INTERNET TIME: <input type='text' name='tijd' value='" + NTP_Time + "'><input type='submit' value='Submit'></form>";
   webPage += "<form action='Auto_Time'>";
   int Auto_Time_OnOff = EEPROM.read(300);
@@ -192,20 +235,25 @@ void Update_web(){
   if (Auto_Time_OnOff == 1){
     webPage += "AUTO TIME ON <input type='radio' name='autotime' value='1' checked><br>";
     webPage += "AUTO TIME OFF <input type='radio' name='autotime' value='0'><br>";
+    webPage += "AUTO UPDATE TIME <input type='text' name='tijd' value='"+ String(EEPROM.read(400)) + ":" + String(EEPROM.read(410)) +"'><br>" ;
   }
   else
   {
     webPage += "AUTO TIME ON <input type='radio' name='mode' value='1' ><br>";
     webPage += "AUTO TIME OFF <input type='radio' name='mode' value='0' checked><br>";
+    webPage += "AUTO UPDATE TIME <input type='text' name='tijd' value='"+ String(EEPROM.read(400)) + ":" + String(EEPROM.read(410)) +"'><br>" ;
   }
   webPage += "<input type='submit' value='Submit'></form>";
   webPage += "The Absolute Display Intensity is: " + Clock_Light_abs + "% <br>"; 
   webPage += "The Relative Display Intensity is: " + Clock_Light_rel + "% <br><br>";
-  webPage += "<form action='LMax'>LIGHT MAX: <input type='text' name='LMAX' value='90'><input type='submit' value='Submit'></form>";
-  webPage += "<form action='LMin'>LIGHT MIN: <input type='text' name='LMIN' value='5'><input type='submit' value='Submit'></form>";
+  webPage += "<form action='LMax'>LIGHT MAX: <input type='text' name='LMAX' value='" + String(Clock_Lmax) + "'><input type='submit' value='Submit'></form>";
+  webPage += "<form action='LMin'>LIGHT MIN: <input type='text' name='LMIN' value='" + String(Clock_Lmin) + "'><input type='submit' value='Submit'></form>";
   webPage += "<form action='TComp'>Tijd Compensatie: <input type='text' name='TCOMP' value='" + Clock_TComp + "'><input type='submit' value='Submit'></form>";
   webPage += "<form action='Modes'>";
+  if (Clock_Mode == "110") 
   webPage += "Uit <input type='radio' name='mode' value='110' checked><br>";
+  else
+  webPage += "Uit <input type='radio' name='mode' value='110' ><br>";
   if (Clock_Mode == "120") 
     webPage += "Waterval <input type='radio' name='mode' value='120' checked><br>";
     else
@@ -229,13 +277,18 @@ void Update_web(){
   webPage += "<input type='submit' value='Submit'></form>";
   webPage += "<form action='ManInp'>MANUAL INPUT: <input type='text' name='ManInput' value=''><input type='submit' value='Submit'></form>";
   webPage += "<form action='update'>Update ESP with new BIN file!<input type='submit' value='UPDATE'></form>";
+ // webPage += "<form action='Test'>DONT PUSH THE TEST BUTTON!<input type='submit' value='TEST'></form>";
   webPage += "<br> " + Debug1 + "<br>" + Debug2 + "<br>" + Debug3;
+  //webPage += "<br> " + String(hour()) + "<br>" + String(minute()) + "<br>" + String(second());
+  //webPage += "<br> " + String(year()) + "<br>" + String(month()) + "<br>" + String(day());
+  //webPage += "<br> " + String(getNTPtime());
 }
 
 
-
 void setup(void){
-  
+  if (EEPROM.read(400) == 255)  EEPROM.write(400,3);
+  if (EEPROM.read(410) == 255)  EEPROM.write(410,0);
+  EEPROM.commit();
   WiFiManager wifiManager;
   wifiManager.autoConnect("AutoConnectAP");
   
@@ -251,40 +304,14 @@ void setup(void){
   //TIME SERVER START
   udp.begin(localPort);
   
-  //Update_web();
-  
   delay(1000);
   Serial.begin(9600);
-  //**WiFi.begin(ssid, password);
-  //Serial.println("");
-
-  // Wait for connection
-  //**while (WiFi.status() != WL_CONNECTED) {
-  //**  delay(500);
-    //Serial.print(".");
-  //**}
-  //Serial.println("");
-  //Serial.print("Connected to ");
-  //Serial.println(ssid);
-  //Serial.print("IP address: ");
-  //Serial.println(WiFi.localIP());
-  
-  //**if (mdns.begin("esp8266", WiFi.localIP())) {
-    //Serial.println("MDNS responder started");
-  //**}
-  int Auto_Time_OnOff = EEPROM.read(300);
-  if (Auto_Time_OnOff == 1){
-    delay(1000); //Wait until clock is finished 
-    getTime();
-    Serial.println("SET TIME " + NTP_Time);
-    delay(1000);
-    Serial.println("SET TIME " + NTP_Time);
-    Debug3 = ("SET TIME " + NTP_Time);
-  }
-  
+  Alarm.alarmRepeat(EEPROM.read(400),EEPROM.read(410),1, UpdateClockTime);
+  InitClock();
   server.on("/", [](){
     getclocksettings();
-    getTime();
+    SetClockTime();
+    FormatTime();
     Update_web();
     server.send(200, "text/html", webPage);
   });
@@ -297,22 +324,128 @@ void setup(void){
   server.on("/TComp", handle_tcomp);
   server.on("/ManInp", handle_maninp);
   server.on("/Auto_Time", handle_autotime);
-    
-  server.begin();
-  //Serial.println("HTTP server started");
+  server.on("/Test", handle_test);
+
+  server.begin(); //("HTTP server started");
 }
  
 void loop(void){
   server.handleClient();
+
 } 
 
-void getTime(){
+void UpdateClockTime(){
+  int Auto_Time_OnOff = EEPROM.read(300);
+  if (Auto_Time_OnOff == 1){
+    SetClockTime();
+    FormatTime();
+    Serial.println("SET TIME " + NTP_Time);
+    Debug3 = "Last Clock Update at :" + String(hour()) + ":" + String(minute()) + ":" + String(second()) + "/" + String(year()) + "-" + String(month()) + "-" + String(day());
+  }
+}
+
+
+
+void InitClock(){
+int Auto_Time_OnOff = EEPROM.read(300);
+  if (Auto_Time_OnOff == 1){
+    delay(1000); //Wait until clock is finished 
+    SetClockTime();
+    FormatTime();
+    Serial.println("SET TIME " + NTP_Time);
+    delay(500);
+    Serial.println("SET TIME " + NTP_Time);
+    Debug3 = ("SET TIME " + NTP_Time);
+  }
+delay(100);
+Clock_Sound = String(EEPROM.read(310));
+if (Clock_Sound == "0") Serial.println("SET SOUND 0");
+delay(100);
+Clock_Notat = String(EEPROM.read(320));
+if (Clock_Notat == "5") Serial.println("SET NOTAT 5");
+delay(100);
+Clock_Lmax = EEPROM.read(330);
+Clock_Lmin = EEPROM.read(340);
+if (Clock_Lmax > Clock_Lmin) Serial.println("SET LMAX 0" + String(Clock_Lmax));
+delay(100);
+if (Clock_Lmax > Clock_Lmin) {
+  if (10 > Clock_Lmin) Serial.println("SET LMIN 00" + String(Clock_Lmin));
+  if (100 > Clock_Lmin) Serial.println("SET LMIN 0" + String(Clock_Lmin));
+}
+int Clock_Mode_INT;
+Clock_Mode_INT = EEPROM.read(350);
+Debug5 = String(Clock_Mode_INT);
+if (170 > Clock_Mode_INT) {
+  Clock_Mode = String(Clock_Mode_INT);
+  Serial.println("SET MODE " + Clock_Mode);
+  Debug4 = Clock_Mode;
+}
+}
+
+
+void SetClockTime(){
+unsigned long Unixtime;
+int var = 0;
+do {
+    delay(200);
+    Unixtime=getNTPtime();
+    var ++;
+    if (var > 5){      // bail out as no ntptime is available now
+       break;
+    }
+    }while (Unixtime==0);
+if (Unixtime >0) {
+  setTime(Unixtime+3600);
+  setTime(Unixtime+adjustDstEurope());
+  }
+}
+
+void FormatTime(){
+  NTP_Time = "";
+    if ( hour() < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      NTP_Time = "0";
+    }
+    NTP_Time = NTP_Time + String(hour()); // print the hour (86400 equals secs per day)
+    NTP_Time = NTP_Time + ":";
+    if ( minute() < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      NTP_Time = NTP_Time + "0";
+    }
+    NTP_Time = NTP_Time + String(minute()); // print the minute (3600 equals secs per minute)
+    NTP_Time = NTP_Time + ":";
+    if ( second() < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+    NTP_Time = NTP_Time + "0";
+    }
+    NTP_Time = NTP_Time + String(second()); // print the second
+}
+
+int adjustDstEurope()
+{
+  // last sunday of march
+  int beginDSTDate=  (31 - (5* year() /4 + 4) % 7);
+  Serial.println(beginDSTDate);
+  int beginDSTMonth=3;
+  //last sunday of october
+  int endDSTDate= (31 - (5 * year() /4 + 1) % 7);
+  Serial.println(endDSTDate);
+  int endDSTMonth=10;
+  // DST is valid as:
+  if (((month() > beginDSTMonth) && (month() < endDSTMonth))
+      || ((month() == beginDSTMonth) && (day() >= beginDSTDate)) 
+      || ((month() == endDSTMonth) && (day() <= endDSTDate)))
+  return 7200;  // DST europe = utc +2 hour
+  else return 3600; // nonDST europe = utc +1 hour
+}
+
+unsigned long getNTPtime(){
     //get a random server from the pool
   WiFi.hostByName(ntpServerName, timeServerIP); 
 
   sendNTPpacket(timeServerIP); // send an NTP packet to a time server
   // wait to see if a reply is available
-  delay(1000);
+  //  delay(1000);
   
   int cb = udp.parsePacket();
   if (!cb) {
@@ -343,31 +476,9 @@ void getTime(){
     unsigned long epoch = secsSince1900 - seventyYears;
     // print Unix time:
     //Serial.println(epoch);
-if (Wintertijd) { epoch = epoch + 3600; } else { epoch = epoch + 7200; }; // wintertijd = +1 uur // zomertijd = +2 uur
-    // print the hour, minute and second:
-    //Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    NTP_Time = "";
-    if ( (((epoch  % 86400L) / 3600)) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      NTP_Time = "0";
-    }
-    NTP_Time = (((epoch  % 86400L) / 3600)); // print the hour (86400 equals secs per day)
-    NTP_Time = NTP_Time + ":";
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      NTP_Time = NTP_Time + "0";
-      //Serial.print('0');
-    }
-    NTP_Time = NTP_Time + ((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    NTP_Time = NTP_Time + ":";
-    //Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-    NTP_Time = NTP_Time + "0";
-      //Serial.print('0');
-    }
-    NTP_Time = NTP_Time + (epoch % 60); // print the second
-  }
+    //epoch = epoch + 7200;
+    return epoch;
+  }   
 }
 
 
@@ -393,4 +504,20 @@ unsigned long sendNTPpacket(IPAddress& address)
   udp.beginPacket(address, 123); //NTP requests are to port 123
   udp.write(packetBuffer, NTP_PACKET_SIZE);
   udp.endPacket();
+}
+
+String getValue(String data, char separator, int index)
+{
+ int found = 0;
+  int strIndex[] = {
+0, -1  };
+  int maxIndex = data.length()-1;
+  for(int i=0; i<=maxIndex && found<=index; i++){
+  if(data.charAt(i)==separator || i==maxIndex){
+  found++;
+  strIndex[0] = strIndex[1]+1;
+  strIndex[1] = (i == maxIndex) ? i+1 : i;
+  }
+ }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
